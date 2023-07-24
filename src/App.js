7cb68e96306382fb45ddd40b72b5cb3385262313
arman <arman.akash20@yahoo.com>
2023-07-24 10:27:09 +0600

    sidebar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DoubleCollapsibleSidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <DoubleCollapsibleSidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MiniDrawer from './MiniDrawer';
+// import ResponsiveDrawer from './ResponsiveDrawer';
+
+
+const Sidebar = () => {
+  return (
+    <>
+      <MiniDrawer />
+      {/* <ResponsiveDrawer /> */}
+    </>
+  );
+};
+
+export default Sidebar;
